b29171b701321e99e307978d77f0ac000257f797
Ash <ashley.bergsma42@gmail.com>
2021-07-09 09:06:46 -0700

    Adds components
    Adds base components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <p>Some stuff!</p>
+      <Home />
+      <About />
+      <Contact />
     </div>
   );
 }
