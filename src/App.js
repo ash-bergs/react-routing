21941640d86384c5623416e249a151fc8bfb54e6
Ash <ashley.bergsma42@gmail.com>
2021-07-09 11:58:42 -0700

    Adds routing
    Brute force, decidedly un-React-ive routing implemented

--- src/App.js
+++ src/App.js
@@ -4,12 +4,34 @@ import Contact from './components/Contact';
 import Home from './components/Home';
 
 function App() {
+  // let's check out the window.location object 👇
+  console.log(window.location);
+
+  // routing functions
+  const showAbout = () => {
+    if (window.location.pathname === '/about') {
+      return <About />;
+    }
+  };
+
+  const showContact = () => {
+    if (window.location.pathname === '/contact') {
+      return <Contact />;
+    }
+  };
+
+  const showHome = () => {
+    if (window.location.pathname === '/') {
+      return <Home />;
+    }
+  };
+
   return (
     <div className="App">
       <h1 className="headline">Routing in React</h1>
-      <Home />
-      <About />
-      <Contact />
+      {showAbout()}
+      {showContact()}
+      {showHome()}
     </div>
   );
 }
