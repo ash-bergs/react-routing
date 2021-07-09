215fdcef817f374532d3be63e0f82a7749f87724
Ash <ashley.bergsma42@gmail.com>
2021-07-09 09:37:26 -0700

    Adds component content
    Some basic content has been added to each component! The stuff here is likely to change, but the basic idea is in place.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
+      <h1>Routing in React</h1>
       <Home />
       <About />
       <Contact />
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div>
-      <p>Contact us</p>
+    <div className="container">
+      <p>
+        Do you have more questions? Just want to get in touch, or have advice on
+        how I can improve this content? Please feel free to reach out to me!
+      </p>
+      <p className="content center">@ash_bergs</p>
+      <p className="content center">bergsma.ash@gmail.com</p>
     </div>
   );
 };
