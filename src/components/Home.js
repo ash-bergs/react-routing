a1c6289daa2ae4a4cfaca716eaef0de2fddb893d
Ash <ashley.bergsma42@gmail.com>
2021-07-13 09:37:32 -0700

    Adds Route component
    Route component added to the project

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,11 @@ const Home = () => {
   return (
     <div className="home container">
       <p className="content">
-        You might already know how to use React Router to set up routing between
-        components. But do you know what's happening under the hood? React
-        Router regularly introduces breaking changes and headaches for devs
-        around the globe. The good new is that a little bit of exploration can
-        help us mend those breaks more quickly and knowledgeably.
+        You've probably used React Router, and know how to set up routing in an
+        app quickly using it. But do you know what's happening under the hood?
+        React Router regularly introduces breaking changes and headaches for
+        devs around the globe. The good new is that a little bit of exploration
+        can help us mend those breaks more quickly and knowledgeably.
       </p>
     </div>
   );
---
+++ src/routing/Route.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Route = ({ children, path }) => {
+  return window.location.pathname === path ? children : null;
+};
+
+export default Route;
