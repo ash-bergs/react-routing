e32113c0a64cc28a1d135384f67a7fe1d29e11f2
Ash <ashley.bergsma42@gmail.com>
2021-07-13 12:39:50 -0700

    Adds Nav component
    Nav bar for the project!

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import About from './components/About';
 import Contact from './components/Contact';
 import Home from './components/Home';
+import Nav from './components/Nav';
 
 import Route from './routing/Route';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1 className="headline">Routing in React</h1>
       <Route path="/">
         <Home />
--- src/routing/Route.js
+++ src/routing/Route.js
@@ -1,7 +1,6 @@
 //import React from 'react';
 
 const Route = ({ children, path }) => {
-  console.log(children);
   return window.location.pathname === path ? children : null;
 };
 
