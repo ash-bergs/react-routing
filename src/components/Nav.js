4afee3c92fc02d89ffc0f74f572775cd494a46b1
Ash <ashley.bergsma42@gmail.com>
2021-07-14 11:36:28 -0700

    Replaces anchor elements
    Replaces anchor elements and adds logic to update the url pathname

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Link from '../routing/Link';
+
 const Nav = () => {
   return (
     <div className="nav">
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/contact">Contact</a>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/contact">Contact</Link>
     </div>
   );
 };
--- src/routing/Link.js
+++ src/routing/Link.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Link = ({ href, className = 'link', children }) => {
+  // custom click handler
+  const onClick = (e) => {
+    e.preventDefault();
+    window.history.pushState({}, '', href);
+  };
+
   return (
-    <a href={href} className={className}>
+    <a onClick={onClick} href={href} className={className}>
       {children}
     </a>
   );
