c925a5115f35033b94f2a1bf802743c378942d4d
Ash <ashley.bergsma42@gmail.com>
2021-07-15 08:32:14 -0700

    Adds event listener to Route component
    Popstate event listener instantiated in Route

--- src/routing/Route.js
+++ src/routing/Route.js
@@ -1,6 +1,20 @@
 //import React from 'react';
+import { useEffect } from 'react';
 
 const Route = ({ children, path }) => {
+  useEffect(() => {
+    // change handler
+    const onPathChange = () => {
+      console.log('Path has been changed!');
+    };
+    // turn on event listener and its handler ⬆
+    window.addEventListener('popstate', onPathChange);
+    // 🧼 anonymous cleanup function
+    return () => {
+      window.removeEventListener('popstate', onPathChange);
+    };
+  }, []);
+
   return window.location.pathname === path ? children : null;
 };
 
